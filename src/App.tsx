bd54ac8ebabf7dc1644a3e5bedaae7535868c496
Hyeonseok <ajy720@gmail.com>
2021-03-29 17:32:16 +0900

    type FC

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Greetings from './Greetings';
+
+const App = () => {
+  const onClick = (name: string) => {
+    console.log(`${name} says hello`)
+  }
+  return <Greetings name="Hello" onClick={onClick} />;
+}
+
+export default App;
