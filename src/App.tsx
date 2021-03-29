8a167363e388bf9ec44416f3561a363789af31b7
Hyeonseok <ajy720@gmail.com>
2021-03-29 18:06:20 +0900

    useState, Input state

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Greetings from './Greetings';
+import Counter from './Counter';
+import MyForm from './MyForm';
 
 const App = () => {
   const onClick = (name: string) => {
     console.log(`${name} says hello`)
   }
-  return <Greetings name="Hello" onClick={onClick} />;
+
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form)
+  }
+
+  return (
+    <Fragment>
+      <Greetings name="Hello" onClick={onClick} />
+      <Counter />
+      <MyForm onSubmit={onSubmit}></MyForm>
+    </Fragment>
+
+  );
 }
 
 export default App;
